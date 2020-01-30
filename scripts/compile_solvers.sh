4fadd6a814393fbc731eefbb36f334d07dcc1cff
John Eslick <john.eslick@netl.doe.gov>
2020-01-30 16:07:36 -0500

    Add clp

--- scripts/compile_solvers.sh
+++ scripts/compile_solvers.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-# Make a directory to work in
+# Set a working directory (I'm assuming this will be the idaes-ext repo dir)
 export IDAES_EXT=`pwd`
 
 # Set a few basic things
 
 export IPOPT_BRANCH="idaes-3.13.0"
 export IPOPT_REPO="https://github.com/idaes/Ipopt"
+
 
 mkdir coinbrew
 cd coinbrew
@@ -15,6 +16,13 @@ bash coinbrew fetch $IPOPT_REPO:$IPOPT_BRANCH --no-prompt
 echo -n >ThirdParty/HSL/.build
 cp -r $IDAES_EXT/coinhsl ThirdParty/HSL/coinhsl
 bash coinbrew build $IPOPT_REPO:$IPOPT_BRANCH --no-prompt
+
+# I think there is an easier way, but I'm going to explicitly do these one-by-one
+bash coinbrew fetch Clp:stable/1.17 --no-prompt
+bash coinbrew build Clp --no-prompt
+
+
+
 
 cd $IDAES_EXT
 mkdir dist-solvers
