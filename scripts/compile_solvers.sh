5008e895c08685325bb3cda35f73c16769ab0ce5
John Eslick <john.eslick@netl.doe.gov>
2020-01-31 08:30:29 -0500

    Add more solvers

--- scripts/compile_solvers.sh
+++ scripts/compile_solvers.sh
@@ -21,13 +21,22 @@ bash coinbrew build $IPOPT_REPO:$IPOPT_BRANCH --no-prompt
 bash coinbrew fetch Clp:stable/1.17 --no-prompt
 bash coinbrew build Clp --no-prompt
 
+bash coinbrew fetch Cbc:stable/2.10 --no-prompt
+bash coinbrew build Cbc --no-prompt
 
+bash coinbrew fetch Bonmin:stable/1.8 --no-prompt
+bash coinbrew build Bonmin --no-prompt
+
+bash coinbrew fetch Couenne:stable/0.5 --no-prompt
+bash coinbrew build Couenne --no-prompt
 
 
 cd $IDAES_EXT
 mkdir dist-solvers
 cd dist-solvers
 cp ../coinbrew/dist/bin/ipopt ./
+cp ../coinbrew/dist/bin/clp ./
+cp ../coinbrew/dist/bin/ipopt_sens ./
 cp ../license.txt ./
 
 # here you pack files
