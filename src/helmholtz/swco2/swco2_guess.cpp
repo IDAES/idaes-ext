cb327475072dd1bc7446daec6f5890c1cd47e3bf
John Eslick <john.eslick@netl.doe.gov>
2020-02-19 20:25:00 -0500

    Add directories

---
+++ src/helmholtz/swco2/swco2_guess.cpp
@@ -0,0 +1,158 @@
+#include"helmholtz_solve.h"
+#include"swco2_param.h"
+
+s_real delta_p_tau_supercritical_guess_swco2(s_real p, s_real tau){
+
+  /* This uses approximate isochors to get a reasonable initial guess for the
+  density solvution for a vapor at a particular temperature and pressure.  If
+  there are tricky regions where a really good guess is needed, a bracketing
+  method can be used to refine the guess.*/
+  s_real T=T_c/tau;
+
+  if(p < 1.38648E-09 + 1.88923E-06*T + 1.14747E-14*T*T){ //rho < 1e-5
+    return 5e-6/rho_c;
+  }
+  else if(p < 1.38648E-08 + 1.88923E-05*T + 1.14747E-13*T*T){ //rho < 1e-4
+    return 5e-5/rho_c;
+  }
+  else if(p < -5.29756E-07 + 0.000188925*T - 5.85444E-14*T*T){ //rho < 1e-3
+    return 5e-4/rho_c;
+  }
+  else if(p < -3.38279E-05 + 0.001889309*T - 2.8997E-11*T*T){ //rho < 1e-2
+    return 5e-3/rho_c;
+  }
+  else if(p < -0.0033456 + 0.018898965*T - 2.60833E-09*T*T){ //rho < 1e-1
+    return 5e-2/rho_c;
+  }
+  else if(p < -0.342149621 + 0.189613095*T - 2.86939E-07*T*T){ //rho < 1
+    return 5e-1/rho_c;
+  }
+  else if(p < -34.14334862 + 1.958265958*T - 2.87239E-05*T*T){ //rho < 10
+    return delta_p_tau_rf(p, tau, 0.5, 15, 0);
+  }
+  else if(p < -2820.065655 + 24.25970886*T - 0.001748359*T*T){ //rho < 100
+    return delta_p_tau_rf(p, tau, 5, 105, 0);
+  }
+  else if(p < -10141.29813 + 56.98420928*T - 0.005166199*T*T){ //rho < 200
+    return delta_p_tau_rf(p, tau, 95, 205, 0);
+  }
+  else if(p < -21125.61421 + 97.22905662*T - 0.008799283*T*T){ //rho < 300
+    return delta_p_tau_rf(p, tau, 195, 305, 0);
+  }
+  else if(p < -35876.37569 + 146.8404454*T - 0.012537284*T*T){ //rho < 400
+    return delta_p_tau_rf(p, tau, 295, 405, 0);
+  }
+  else if(p < -54964.94489 + 209.5598894*T - 0.017302282*T*T){ //rho < 500
+    return delta_p_tau_rf(p, tau, 395, 505, 0);
+  }
+  else if(p < -79898.47714 + 293.2504079*T - 0.026823627*T*T){ //rho < 600
+    return delta_p_tau_rf(p, tau, 495, 605, 0);
+  }
+  else if(p < -112368.0377 + 408.529043*T - 0.046634364*T*T){ //rho < 700
+    return delta_p_tau_rf(p, tau, 595, 705, 0);
+  }
+  else if(p < -153093.0031 + 566.7194992*T - 0.082889317*T*T){ //rho < 800
+    return delta_p_tau_rf(p, tau, 695, 805, 0);
+  }
+  else if(p < -202040.1487 + 782.056835*T - 0.144729326*T*T){ //rho < 900
+    return delta_p_tau_rf(p, tau, 795, 905, 0);
+  }
+  return 1100/rho_c;
+}
+
+s_real delta_p_tau_vap_guess_swco2(s_real p, s_real tau){
+
+  /* This uses approximate isochors to get a reasonable initial guess for the
+  density solvution for a vapor at a particular temperature and pressure.  If
+  there are tricky regions where a really good guess is needed, a bracketing
+  method can be used to refine the guess.*/
+  s_real T=T_c/tau;
+
+  if (p >= P_c && tau <= 1){
+    return delta_p_tau_supercritical_guess_swco2(p, tau);
+  }
+  else if(p > 5068.8 && p <= P_c && T > 288.0 && tau > 1.0){
+    //This area is at the top end of the saturation curve near the critical
+    //point. Know the vapor density is <= critical density, and know the lowest
+    //possible density in this box, so use bracketing method to get a very close
+    //guess.
+    return delta_p_tau_rf(p, tau, 150, rho_c, 0);
+  }
+  // for the rest just use a guess between isochors
+  else if(p < 1.38648E-09 + 1.88923E-06*T + 1.14747E-14*T*T){ //rho < 1e-5
+    return 5e-6/rho_c;
+  }
+  else if(p < 1.38648E-08 + 1.88923E-05*T + 1.14747E-13*T*T){ //rho < 1e-4
+    return 5e-5/rho_c;
+  }
+  else if(p < -5.29756E-07 + 0.000188925*T - 5.85444E-14*T*T){ //rho < 1e-3
+    return 5e-4/rho_c;
+  }
+  else if(p < -3.38279E-05 + 0.001889309*T - 2.8997E-11*T*T){ //rho < 1e-2
+    return 5e-3/rho_c;
+  }
+  else if(p < -0.0033456 + 0.018898965*T - 2.60833E-09*T*T){ //rho < 1e-1
+    return 5e-2/rho_c;
+  }
+  else if(p < -0.342149621 + 0.189613095*T - 2.86939E-07*T*T){ //rho < 1
+    return 5e-1/rho_c;
+  }
+  else if(p < -34.14334862 + 1.958265958*T - 2.87239E-05*T*T){ //rho < 10
+    return delta_p_tau_rf(p, tau, 0.5, 15, 0);
+  }
+  else if(p < -2820.065655 + 24.25970886*T - 0.001748359*T*T){ //rho < 100
+    return delta_p_tau_rf(p, tau, 5, 105, 0);
+  }
+  else if(p < -10141.29813 + 56.98420928*T - 0.005166199*T*T){ //rho < 200
+    return delta_p_tau_rf(p, tau, 95, 205, 0);
+  }
+  else if(p < -21125.61421 + 97.22905662*T - 0.008799283*T*T){ //rho < 300
+    return delta_p_tau_rf(p, tau, 195, 305, 0);
+  }
+  else if(p < -35876.37569 + 146.8404454*T - 0.012537284*T*T){ //rho < 400
+    return delta_p_tau_rf(p, tau, 295, 405, 0);
+  }
+  return delta_p_tau_rf(p, tau, 395, rho_c, 0);
+}
+
+s_real delta_p_tau_liq_guess_swco2(s_real p, s_real tau){
+  /* This uses approximate isochors to get a reasonable initial guess for the
+  density solvution for a vapor at a particular temperature and pressure.  If
+  there are tricky regions where a really good guess is needed, a bracketing
+  method can be used to refine the guess.*/
+  s_real T=T_c/tau;
+
+  if (p >= P_c && tau <= 1){
+    return delta_p_tau_supercritical_guess_swco2(p, tau);
+  }
+  else if(p > 5068.8 && p <= P_c && T > 288.0 && tau > 1.0){
+    //This area is at the top end of the saturation curve near the critical
+    //point. Know the vapor density is <= critical density, and know the lowest
+    //possible density in this box, so use bracketing method to get a very close
+    //guess.
+    return delta_p_tau_rf(p, tau, rho_c, 850, 0);
+  }
+  // for the rest just use a guess between isochors
+  else if(p < -54964.94489 + 209.5598894*T - 0.017302282*T*T){ //rho < 500
+    return delta_p_tau_rf(p, tau, rho_c, 505, 0);
+  }
+  else if(p < -79898.47714 + 293.2504079*T - 0.026823627*T*T){ //rho < 600
+    return delta_p_tau_rf(p, tau, 495, 605, 0);
+  }
+  else if(p < -112368.0377 + 408.529043*T - 0.046634364*T*T){ //rho < 700
+    return delta_p_tau_rf(p, tau, 595, 705, 0);
+  }
+  else if(p < -153093.0031 + 566.7194992*T - 0.082889317*T*T){ //rho < 800
+    return delta_p_tau_rf(p, tau, 695, 805, 0);
+  }
+  else if(p < -202040.1487 + 782.056835*T - 0.144729326*T*T){ //rho < 900
+    return delta_p_tau_rf(p, tau, 795, 905, 0);
+  }
+  else if(p < -256602.6793 + 1066.954274*T - 0.241727489*T*T){ //rho < 1000
+    return delta_p_tau_rf(p, tau, 895, 1005, 0);
+  }
+  else if(p < -312602.0898 + 1436.311945*T - 0.38689172*T*T){ //rho < 1100
+    return delta_p_tau_rf(p, tau, 995, 1105, 0);
+  }
+  return 1170/rho_c;
+}
---
+++ src/helmholtz/swco2/swco2_guess.h
@@ -0,0 +1,9 @@
+
+#ifndef _INCLUDE_SWCO2_GUESS_H_
+#define _INCLUDE_SWCO2_GUESS_H_
+
+s_real delta_p_tau_supercritical_guess_swco2(s_real p, s_real tau);
+s_real delta_p_tau_vap_guess_swco2(s_real p, s_real tau);
+s_real delta_p_tau_liq_guess_swco2(s_real p, s_real tau);
+
+ #endif
