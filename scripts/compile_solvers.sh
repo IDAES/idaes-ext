5bc3c387841a2d0e25d315d8c79de7819cba2aa5
John Eslick <john.eslick@netl.doe.gov>
2020-03-24 21:03:27 -0400

    Try again on static link ipopt libraries

--- scripts/compile_solvers.sh
+++ scripts/compile_solvers.sh
@@ -28,7 +28,7 @@ then
 else
   echo "HSL Not Available, BUILDING WITHOUT HSL" >&2
 fi
-bash coinbrew build $IPOPT_REPO:$IPOPT_BRANCH --no-prompt --enable-static --disable-shared
+bash coinbrew build $IPOPT_REPO:$IPOPT_BRANCH --no-prompt --enable-static
 
 cd $IDAES_EXT
 mkdir dist-solvers
