e5b207f4538a36612f45c5e5e8c5c466c41c2c8e
John Eslick <john.eslick@netl.doe.gov>
2020-03-26 13:54:48 -0400

    Catch copy error if solvers not built

--- docker/build-windows.ps1
+++ docker/build-windows.ps1
@@ -15,6 +15,11 @@ docker build --rm -t ${flavor}_build .
 docker run --name ${flavor}_build_tmp -dt ${flavor}_build:latest
 docker stop ${flavor}_build_tmp
 docker cp ${flavor}_build_tmp:${repo}/idaes-ext/dist-lib/idaes-lib-${flavor}-64.tar.gz .
-docker cp ${flavor}_build_tmp:${repo}/idaes-ext/dist-solvers/idaes-solvers-${flavor}-64.tar.gz .
+try{
+  docker cp ${flavor}_build_tmp:${repo}/idaes-ext/dist-solvers/idaes-solvers-${flavor}-64.tar.gz .
+}
+catch{
+  echo "Solvers were not built."
+}
 docker rm ${flavor}_build_tmp
 cd ..
