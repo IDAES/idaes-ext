5bfaf362c1ea17f30b740e2e8301a7e85ed66b1b
John Eslick <john.eslick@netl.doe.gov>
2020-04-22 15:45:01 -0400

    More script fixing

--- scripts/compile_libs.sh
+++ scripts/compile_libs.sh
@@ -32,6 +32,19 @@ cd dist-lib
 cp ../src/dist/*.so ./
 cp ../license.txt ./
 
+if [ "$(expr substr $(uname -s) 1 7)" == "MINGW64" ]
+then
+    # Winodws MinGW linked libraries
+    cp /mingw64/bin/libstdc++-6.dll ./
+    cp /mingw64/bin/libgcc_s_seh-1.dll ./
+    cp /mingw64/bin/libwinpthread-1.dll ./
+    cp /mingw64/bin/libgfortran-4.dll ./
+    cp /mingw64/bin/libquadmath-0.dll ./
+    cp /mingw64/bin/libgomp-1.dll ./
+    cp /mingw64/bin/liblapack.dll ./
+    cp /mingw64/bin/libblas.dll ./
+fi
+
 # Compile Pynumero
 
 cd $IDAES_EXT
@@ -47,25 +60,16 @@ make
 cd ../../../
 mkdir build
 cd build
-cmake ..
-make
-cp asl_interface/libpynumero_ASL* $IDAES_EXT/dist-lib
-cp sparse_utils/libpynumero_SPARSE* $IDAES_EXT/dist-lib
-
-
-
 if [ "$(expr substr $(uname -s) 1 7)" == "MINGW64" ]
 then
-    # Winodws MinGW linked libraries
-    cp /mingw64/bin/libstdc++-6.dll ./
-    cp /mingw64/bin/libgcc_s_seh-1.dll ./
-    cp /mingw64/bin/libwinpthread-1.dll ./
-    cp /mingw64/bin/libgfortran-4.dll ./
-    cp /mingw64/bin/libquadmath-0.dll ./
-    cp /mingw64/bin/libgomp-1.dll ./
-    cp /mingw64/bin/liblapack.dll ./
-    cp /mingw64/bin/libblas.dll ./
+  cmake -G"MSYS Makefiles" ..
+else
+  cmake ..
 fi
+make
+cp asl_interface/libpynumero_ASL* $IDAES_EXT/dist-lib
+#cp sparse_utils/libpynumero_SPARSE* $IDAES_EXT/dist-lib
+cd $IDAES_EXT/dist-lib
 
 # here you pack files
 tar -czvf idaes-lib-${osname}-64.tar.gz *
