1b20679f4566dacb137ce118e7e5f02980e637b7
John Eslick <john.eslick@netl.doe.gov>
2020-05-12 11:04:20 -0400

    call func in memo test

--- src/helmholtz/common/helmholtz_solve.cpp
+++ src/helmholtz/common/helmholtz_solve.cpp
@@ -334,6 +334,9 @@ s_real sat_tau_with_derivs(s_real pr, s_real *grad, s_real *hes, int *nit){
 
 s_real mem_tau_with_derivs(s_real ht, s_real pr, s_real *grad, s_real *hes){
     // TESTING ONLY, function for testing memoization
+    s_real val
+    val = tau_with_derivs(ht, pr, grad, hes);
+    //return the memoized value
     return memoize::get_bin(memoize::TAU_FUNC, ht, pr, grad, hes);
 }
 
