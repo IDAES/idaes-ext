15e92fcaa64a4383d9bbd00c0173117188e6e891
John Eslick <john.eslick@netl.doe.gov>
2020-05-14 22:28:05 -0400

    fix step

--- src/helmholtz/common/helmholtz_solve.cpp
+++ src/helmholtz/common/helmholtz_solve.cpp
@@ -230,7 +230,7 @@ int newton_2d(FuncWrapper *f0, FuncWrapper *f1, s_real x00, s_real x10,
       Jinv[1][0] = -grad1[0]/det; // J[1][0] = grad1[0]
       Jinv[1][1] =  grad0[0]/det; // J[1][0] = grad1[1]
       x0 = Jinv[0][0]*fun0 + Jinv[0][1]*fun1;
-      x1 = Jinv[1][0]*fun0 + Jinv[1][0]*fun1;
+      x1 = Jinv[1][0]*fun0 + Jinv[1][1]*fun1;
       fun0 = (*f0)(x0, x1, grad0, hes0);
       fun1 = (*f1)(x0, x1, grad1, hes1);
       ++it;
