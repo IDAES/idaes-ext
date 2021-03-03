7b612aa98459214d58c3c05c55ac7ab8409313c6
John Eslick <john.eslick@netl.doe.gov>
2021-03-03 09:28:15 -0500

    Update functions.c

--- src/functions/functions.c
+++ src/functions/functions.c
@@ -13,10 +13,30 @@ void funcadd(AmplExports *ae){
 }
 
 extern real scbrt(arglist *al){
+    // al is the argument list data structure
+    // al->ra is an array real arguments
+    // al->at is an array of of argument positions
+    // The reason for using al->at for the position is 
+    // that there could also be integer or string 
+    // arguments
     real x = al->ra[al->at[0]];
+    
+    //al->derivs is a pointer to an array of first derivatives
+    //of the function with respect to each real arg if 
+    //derivs is NULL solver isn't requesting derivatives.
     if(al->derivs!=NULL){
       if(fabs(x) < 6e-9) al->derivs[0] = 1e5;
       else al->derivs[0] = pow(cbrt(x), -2.0)/3.0;
+      //al->hes is a pointer the Hessian matrix if NULL second
+      //derivatives are not requested.  This function takes a
+      //single argument, but the Hessian form is upper triangle
+      //column-major like
+      // Args Index  0 1 2 3 ...
+      //          0  0 1 3 6
+      //          1    2 4 7
+      //          2      5 8
+      //          3        9
+      //        ...
       if(al->hes!=NULL){
         al->hes[0] = -2.0*pow(cbrt(x), -5.0)/9.0;
       }
