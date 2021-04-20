bf81ff16e9b1b89df008a369023680c7ab2f6df8
John Eslick <john.eslick@netl.doe.gov>
2021-04-20 15:06:03 -0400

    Fix messed up header file.

--- src/functions/functions.h
+++ src/functions/functions.h
@@ -8,18 +8,6 @@
 
 
 real scbrt(arglist *al);
-
-#endif
-#include "funcadd.h"
-
-#ifndef _INCLUDE_FUNCTIONS_H_
-#define _INCLUDE_FUNCTIONS_H_
-
-#include <time.h>
-#include <math.h>
-
-
-real scbrt(arglist *al);
 real testing_only(arglist *al);
 
 #endif
