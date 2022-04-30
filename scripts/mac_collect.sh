a2ce075c796b30e890860c696ad9f7688c5fce91
John Eslick <john.eslick@netl.doe.gov>
2022-04-30 09:36:43 -0500

    Fix script typo

--- scripts/mac_collect.sh
+++ scripts/mac_collect.sh
@@ -1,3 +1,3 @@
-mv ./dist-solvers/idaes-solvers-darwin-arm64.tar.gz ./idaes-slovers-darwin-aarch64.tar.gz
+mv ./dist-solvers/idaes-solvers-darwin-arm64.tar.gz ./idaes-solvers-darwin-aarch64.tar.gz
 mv ./dist-lib/idaes-lib-darwin-arm64.tar.gz ./idaes-lib-darwin-aarch64.tar.gz
 mv ./dist-petsc/idaes-petsc-darwin-arm64.tar.gz ./idaes-petsc-darwin-aarch64.tar.gz
