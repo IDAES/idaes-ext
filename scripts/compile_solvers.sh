61ce4b893854bdd50f57a3a20a39ff2819be1318
John Eslick <john.eslick@netl.doe.gov>
2022-05-09 19:53:19 -0400

    try again

--- scripts/compile_solvers.sh
+++ scripts/compile_solvers.sh
@@ -186,6 +186,7 @@ if [ ${osname} = "windows" ]; then
     --with-mumps-lflags="-L$PETSC_DIR/lib" \
     --with-mumps-cflags="-I$PETSC_DIR/include -I$PETSC_DIR/include/mumps_libseq" \
     --prefix=$IDAES_EXT/coinbrew/dist
+    LDFLAGS="-L$PETSC_DIR/lib -lmetis -ldmumps"
 else
   ./configure --disable-shared --enable-static --with-mumps \
     --with-mumps-lflags="-L$PETSC_DIR/lib -lmetis -ldmumps -lmumps_common -lmpiseq -lpord" \
