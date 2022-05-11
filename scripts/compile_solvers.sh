7349225dc0b1f27da9a7f2c78c0a60dcfe61d09d
John Eslick <john.eslick@netl.doe.gov>
2022-05-10 22:55:13 -0500

    Fix

--- scripts/compile_solvers.sh
+++ scripts/compile_solvers.sh
@@ -136,13 +136,12 @@ echo "# Ipopt ampl executables                                                #"
 echo "#########################################################################"
 cd Ipopt
 if [ ${osname} = "el7" ]; then
-  cp $IDAES_EXT/coinbrew/dist/include/coin-or/hsl/* $IDAES_EXT/coinbrew/dist/include/coin-or/
   ./configure --disable-shared --enable-static --with-mumps \
+    --with-hsl-lflags="-I$IDAES_EXT/coinbrew/dist/include/coin-or/hsl" \
     --with-hsl-lflags="-L$PETSC_DIR/lib -lmetis" \
     --with-mumps-lflags="-L$PETSC_DIR/lib -lmetis" \
     --with-mumps-cflags="-I$PETSC_DIR/include -I$PETSC_DIR/include/mumps_libseq" \
     --prefix=$IDAES_EXT/coinbrew/dist \
-    ADD_CXXFLAGS="-I$IDAES_EXT/coinbrew/dist/coin-or/hsl" \
     LDFLAGS="-L$PETSC_DIR/lib -lgfortran -lpthread -lmetis -ldmumps -lmumps_common -lmpiseq -lpord"
 else
 ./configure --disable-shared --enable-static --with-mumps \
