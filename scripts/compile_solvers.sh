4fe0a238e43823796d675536cd145b860d94533f
John Eslick <john.eslick@netl.doe.gov>
2022-05-10 21:52:44 -0500

    Fix

--- scripts/compile_solvers.sh
+++ scripts/compile_solvers.sh
@@ -137,6 +137,7 @@ echo "#########################################################################"
 cd Ipopt
 if [ ${osname} = "el7" ]; then
   ./configure --disable-shared --enable-static --with-mumps \
+    --with-hsl-lflags="-L$PETSC_DIR/lib -lmetis" \
     --with-mumps-lflags="-L$PETSC_DIR/lib -lmetis" \
     --with-mumps-cflags="-I$PETSC_DIR/include -I$PETSC_DIR/include/mumps_libseq" \
     --prefix=$IDAES_EXT/coinbrew/dist \
