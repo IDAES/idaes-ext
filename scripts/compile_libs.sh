4786eebc0eaaf6bfe3d5ec6bd0e5f40a2066bf6f
John Eslick <john.eslick@netl.doe.gov>
2022-08-12 16:33:50 -0500

    Fix

--- scripts/compile_libs.sh
+++ scripts/compile_libs.sh
@@ -23,7 +23,7 @@ cd $IDAES_EXT/src
 if [ ${osname} = "darwin" ]; then
   export BOOST_HEADER=/opt/homebrew/include
 fi
-if [ ${osname} = "darwin" ]; then
+if [ ${osname} = "windows" ]; then
   export PATH=$PATH:$IDAES_EXT/coinbrew/dist/lib64
 fi
 export LD_LIBRARY_PATH=$IDAES_EXT/coinbrew/dist/lib64
