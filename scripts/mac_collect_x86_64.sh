2916fbbc82304afca0b8cf8d820f2fab684d8754
John Eslick <john.eslick@netl.doe.gov>
2023-02-17 14:24:44 -0600

    Create mac_collect_x86_64.sh

---
+++ scripts/mac_collect_x86_64.sh
@@ -0,0 +1,3 @@
+mv ./dist-solvers/idaes-solvers-darwin-arm64.tar.gz ./idaes-solvers-darwin-x86_64.tar.gz
+mv ./dist-lib/idaes-lib-darwin-arm64.tar.gz ./idaes-lib-darwin-x86_64.tar.gz
+mv ./dist-petsc/idaes-petsc-darwin-arm64.tar.gz ./idaes-petsc-darwin-x86_64.tar.gz
