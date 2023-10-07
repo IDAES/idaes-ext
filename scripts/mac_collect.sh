44a82154edac757ee381cba3530f5ebd9a1123aa
robbybp <robbybparker@gmail.com>
2023-10-06 19:18:44 -0600

    update mac_collect for new tar file names

--- scripts/mac_collect.sh
+++ scripts/mac_collect.sh
@@ -1,3 +1,2 @@
 mv ./dist-solvers/idaes-solvers-darwin-arm64.tar.gz ./idaes-solvers-darwin-aarch64.tar.gz
-mv ./dist-lib/idaes-lib-darwin-arm64.tar.gz ./idaes-lib-darwin-aarch64.tar.gz
-mv ./dist-petsc/idaes-petsc-darwin-arm64.tar.gz ./idaes-petsc-darwin-aarch64.tar.gz
+mv ./dist-lib/idaes-functions-darwin-arm64.tar.gz ./idaes-lib-darwin-aarch64.tar.gz
