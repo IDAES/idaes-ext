d55d9a0688b7f62cd9d134130d7b542b5e7daf38
robbybp <robbybparker@gmail.com>
2023-10-06 19:21:28 -0600

    update mac_collect

--- scripts/mac_collect.sh
+++ scripts/mac_collect.sh
@@ -1,2 +1,2 @@
-mv ./dist-solvers/idaes-solvers-darwin-arm64.tar.gz ./idaes-solvers-darwin-aarch64.tar.gz
-mv ./dist-lib/idaes-functions-darwin-arm64.tar.gz ./idaes-lib-darwin-aarch64.tar.gz
+mv ./dist/idaes-solvers-darwin-arm64.tar.gz ./idaes-solvers-darwin-aarch64.tar.gz
+mv ./dist-functions/idaes-functions-darwin-arm64.tar.gz ./idaes-functions-darwin-aarch64.tar.gz
--- scripts/mac_collect_x86_64.sh
+++ scripts/mac_collect_x86_64.sh
@@ -1,3 +1,2 @@
-mv ./dist-solvers/idaes-solvers-darwin-x86_64.tar.gz ./idaes-solvers-darwin-x86_64.tar.gz
-mv ./dist-lib/idaes-lib-darwin-x86_64.tar.gz ./idaes-lib-darwin-x86_64.tar.gz
-mv ./dist-petsc/idaes-petsc-darwin-x86_64.tar.gz ./idaes-petsc-darwin-x86_64.tar.gz
+mv ./dist/idaes-solvers-darwin-x86_64.tar.gz ./idaes-solvers-darwin-x86_64.tar.gz
+mv ./dist-functions/idaes-functions-darwin-x86_64.tar.gz ./idaes-functions-darwin-x86_64.tar.gz
